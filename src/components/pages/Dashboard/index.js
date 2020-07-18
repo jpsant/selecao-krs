c90967675ddd75211447c7857be24ce82008f26c
João Paulo <j.paaulinho@gmail.com>
2020-07-17 23:37:12 -0300

    created the Pages of the application

---
+++ src/components/pages/Dashboard/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./styles.scss";
+
+import Header from "../../ui/organisms/Header";
+import CardsContainer from "../../ui/organisms/CardsContainer";
+
+export default function Dashboard() {
+
+  return (
+    <>
+      <Header />
+      <CardsContainer />
+    </>
+  );
+}
