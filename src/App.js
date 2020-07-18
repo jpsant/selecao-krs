491df7cd4c50b60a78929f1d16018530f57b640f
João Paulo <j.paaulinho@gmail.com>
2020-07-17 23:37:59 -0300

    creating the application store and the global stylesheet

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Dashboard from './components/pages/Dashboard';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello world!</h1>
+      <Dashboard />
     </div>
   );
 }
--- src/store/reducers/store.js
+++ src/store/reducers/store.js
@@ -1,4 +1,4 @@
-import * as actionTypes from "../actions/actiontypes";
+import * as actionTypes from "../actions/actionTypes";
 import { updateObject } from "../ultility";
 
 const initialState = {
