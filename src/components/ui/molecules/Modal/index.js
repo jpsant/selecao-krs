2795a1b92c1c105b3e4be99f08e1ea61ffc12610
João Paulo <j.paaulinho@gmail.com>
2020-07-18 12:23:20 -0300

    added the dynamic showing to the modal

--- src/components/ui/molecules/Modal/index.js
+++ src/components/ui/molecules/Modal/index.js
@@ -1,15 +1,19 @@
-import React from 'react';
-import './styles.scss';
+import React from "react";
+import "./styles.scss";
 
-import ConfirmButton from '../../../ui/atoms/ConfirmButton';
-import CloseButton from '../../../ui/atoms/CloseButton'
+import CloseButton from "../../../ui/atoms/CloseButton";
 
-export default function Modal({fnc, children}) {
+export default function Modal({ children, close, show }) {
   return (
-    <div className="modal">
-      <CloseButton />
+    <div
+      style={{
+        visibility: show ? "visible" : "hidden",
+        opacity: show ? "1" : "0",
+      }}
+      className="modal"
+    >
+      <CloseButton fnc={close} />
       {children}
-      <ConfirmButton text="Confirmar" />
     </div>
-  )
+  );
 }
