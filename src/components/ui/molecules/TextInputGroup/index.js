8c47d3da0ea107637a42dd555ff162accbba331e
João Paulo <j.paaulinho@gmail.com>
2020-07-18 23:21:37 -0300

    modified the inputgroup to TextInputGropu

---
+++ src/components/ui/molecules/TextInputGroup/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './styles.scss'
+
+import Label from '../../atoms/Label';
+import Input from '../../atoms/Input';
+
+export default function InputGroup({name, fnc, width, required}) {
+  return (
+    <div className="inputGroup">
+      <Label name={name} />
+      <Input required={required} width={width} fnc={text => fnc(text)} name={name} />
+    </div>
+  )
+}
